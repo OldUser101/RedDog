4c92fc0b4c9d689bb90d1c3f899a55f72ebd5955
Nathan Gill <nathan.j.gill@outlook.com>
2025-03-04 11:26:30 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RedDog
+{
+    internal class Program
+    {
+        public static void ShowTitle() 
+        {
+            Console.Write("=== ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("RED DOG");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(" ===");
+            Console.WriteLine("(c) Nathan Gill");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            ShowTitle();
+        }
+    }
+}
